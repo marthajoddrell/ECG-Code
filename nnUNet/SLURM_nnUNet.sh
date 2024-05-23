e97f62d070105874ed201fc65eec47227cee5e1d
marthajoddrell <75280585+marthajoddrell@users.noreply.github.com>
2024-05-23 10:50:21 +0100

    Create SLURM_nnUNet.sh

---
+++ nnUNet/SLURM_nnUNet.sh
@@ -0,0 +1,121 @@
+#!/bin/bash -l
+# Use the current working directory
+#SBATCH -D ./
+
+# Use the current environment for this job
+#SBATCH --export=All
+
+# Define job name
+#SBATCH  -J nnUNet_run
+
+# Define a standard output file. When the job is running, %N will be replaced by the name of
+# the first node where the job runs, %j will be replaced by job id number.
+#SBATCH -o slurm.%N.%j.out
+
+# Define a standard error file
+#SBATCH -e slurm.%N.%j.err
+
+# Request the GPU partition (gpulowsmall, or gpudacdt/gputroisi if you have dedicated access). We don't recommend requesting multiple partitions, as the specifications of the nodes in these partitions are different.
+#SBATCH -p gpulowsmall
+
+# Request the number of nodes
+#SBATCH -N 1
+
+# Request the number of GPUs per node to be used (if more than 1 GPU per node is required, change 1 into Ngpu, where Ngpu=2)
+#SBATCH --gres=gpu:1
+
+# Request the number of CPU cores. (There are 48 CPU cores and 2 GPUs on each GPU node in partition gpulowsmall/gpudacdt/gputroisi,
+# so please request 24*Ngpu CPU cores, i.e., 24 CPU cores for 1 GPU, 48 CPU cores for 2 GPUs).
+# SBATCH -n 1
+#SBATCH --ntasks-per-node=2    # total number of tasks per node
+#SBATCH --cpus-per-task=24
+
+# Set time limit in format a-bb:cc:dd, where a is days, b is hours, c is minutes, and d is seconds.
+# SBATCH -t 5-00:00:00
+
+# Request the memory on the node or request memory per core
+# PLEASE don't set the memory option as we should use the default memory which is based on the number of cores
+# SBATCH --mem=90GB or #SBATCH --mem-per-cpu=9000M
+
+# Insert your own username to get e-mail notifications (note: keep just one "#" before SBATCH)
+##SBATCH --mail-user=
+
+# Notify user by email when certain event types occur
+#SBATCH --mail-type=ALL
+#s
+# Set your maximum stack size to unlimited
+ulimit -s unlimited
+
+# export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4) + $SLURM_ARRAY_TASK_ID)
+# Load tensorflow and relevant modules
+module purge
+
+module load apps/anaconda3/2023.03-poetry
+source activate nnunet
+
+# Load MPI module
+module load mpi
+
+module list
+
+echo =========================================================
+echo "mpiexec=`which mpiexec`"
+echo "mpirun=`which mpirun`"
+echo SLURM job: submitted  date = `date`
+date_start=`date +%s`
+
+# echo Executable file:                              
+# echo MPI parallel job.                                  
+# echo -------------  
+# echo Job output begins                                           
+# echo -----------------                                           
+# echo
+echo "CUDA_VISIBLE_DEVICES : $CUDA_VISIBLE_DEVICES"
+echo "GPU_DEVICE_ORDINAL   : $GPU_DEVICE_ORDINAL"
+hostname
+echo Current directory: `pwd`
+echo "Print the following environmetal variables:"
+echo "Job name                     : $SLURM_JOB_NAME"
+echo "Job ID                       : $SLURM_JOB_ID"
+echo "Job user                     : $SLURM_JOB_USER"
+echo "Job array index              : $SLURM_ARRAY_TASK_ID"
+echo "Submit directory             : $SLURM_SUBMIT_DIR"
+echo "Temporary directory          : $TMPDIR"
+echo "Submit host                  : $SLURM_SUBMIT_HOST"
+echo "Queue/Partition name         : $SLURM_JOB_PARTITION"
+echo "Node list                    : $SLURM_JOB_NODELIST"
+echo "Hostname of 1st node         : $HOSTNAME"
+echo "Number of nodes allocated    : $SLURM_JOB_NUM_NODES or $SLURM_NNODES"
+echo "Number of processes          : $SLURM_NTASKS"
+echo "Number of processes per node : $SLURM_TASKS_PER_NODE"
+echo "Requested tasks per node     : $SLURM_NTASKS_PER_NODE"
+echo "Requested CPUs per task      : $SLURM_CPUS_PER_TASK"
+echo "Scheduling priority          : $SLURM_PRIO_PROCESS"
+echo "OMP_NUM_THREADS              : $SLURM_CPUS_ON_NODE"
+echo "OMPI_COMM_WORLD_SIZE         : $OMPI_COMM_WORLD_SIZE"
+echo "OMPI_COMM_WORLD_RANK         : $OMPI_COMM_WORLD_RANK"
+echo "SLURM_NTASKS                 : $SLURM_NTASKS"
+echo "proc_id                      : $SLURM_PROCID"
+echo "node_list                    : $SLURM_NODELIST"
+
+echo "Running parallel job:"
+
+# Set environment variables
+export nnUNet_raw="/sharedscratch/nnUNet/nnUNet_raw"
+export nnUNet_results="/sharedscratch/nnUNet/nnUNet_results"
+export nnUNet_preprocessed="/sharedscratch/nnUNet/nnUNet_preprocessed"
+
+# Plan and preprocess
+nnUNetv2_train 139 2d 4
+conda deactivate
+
+date_end=`date +%s`
+seconds=$((date_end-date_start))
+minutes=$((seconds/60))
+seconds=$((seconds-60*minutes))
+hours=$((minutes/60))
+minutes=$((minutes-60*hours))
+echo =========================================================
+echo SLURM job: finished   date = `date`
+echo Total run time : $hours Hours $minutes Minutes $seconds Seconds
+echo =========================================================
